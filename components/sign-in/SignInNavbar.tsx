484577989d9f524fa6cf802e6244bee8d1fcf65a
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-01-26 00:34:24 +0530

    Update SignInNavbar component styling

--- components/sign-in/SignInNavbar.tsx
+++ components/sign-in/SignInNavbar.tsx
@@ -3,8 +3,13 @@ import Logo from "./Logo";
 
 const SignInNavbar = () => {
   return (
-    <nav className="lg:hidden w-full bg-color-brand min-h-20">
+    <nav
+      className="lg:hidden w-full bg-color-brand min-h-20 flex 
+      justify-start items-center px-4 py-2 gap-3
+    "
+    >
       <Logo className="w-7 h-7 " />
+      <p className="text-[20px] text-white font-nunito font-semibold">Base</p>
     </nav>
   );
 };
