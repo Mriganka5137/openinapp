b7124e29c6b7dcbecfe059f13abf0dc844e6fb04
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-01-27 00:06:46 +0530

    Add redirects to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/sign-in",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
